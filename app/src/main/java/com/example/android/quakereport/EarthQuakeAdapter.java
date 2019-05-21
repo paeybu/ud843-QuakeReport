00616849bfd57abf77fbc4684fefc3fbe25db2bd
Vachirarat Thavisaengsiri <pae9769@gmail.com>
2019-05-21 20:04:16 +0700

    add circle color depending on mag

--- app/src/main/java/com/example/android/quakereport/EarthQuakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthQuakeAdapter.java
@@ -1,8 +1,11 @@
 package com.example.android.quakereport;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,7 @@ import java.text.DecimalFormat;
 import java.util.List;
 
 public class EarthQuakeAdapter extends ArrayAdapter<Earthquake> {
+    private static final String LOG_TAG = EarthQuakeAdapter.class.getSimpleName();
     private Context mContext;
     public EarthQuakeAdapter(@NonNull Context context, int resource, @NonNull List<Earthquake> objects) {
         super(context, resource, objects);
@@ -26,7 +30,7 @@ public class EarthQuakeAdapter extends ArrayAdapter<Earthquake> {
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
         }
-        Earthquake earthQuake = getItem(position);
+        Earthquake currentEarthquake = getItem(position);
 
         TextView magTv = (TextView) listItemView.findViewById(R.id.magTv);
         TextView offsetTv = (TextView) listItemView.findViewById(R.id.offsetTv);
@@ -34,12 +38,12 @@ public class EarthQuakeAdapter extends ArrayAdapter<Earthquake> {
         TextView dateTv = (TextView) listItemView.findViewById(R.id.dateTv);
         TextView timeTv = (TextView) listItemView.findViewById(R.id.timeTv);
 
-        Double magnitude = earthQuake.getMagnitude();
+        Double magnitude = currentEarthquake.getMagnitude();
         DecimalFormat formatter = new DecimalFormat("0.0");
         String magnitudeString = formatter.format(magnitude);
 
         magTv.setText(magnitudeString);
-        String place = earthQuake.getPlace();
+        String place = currentEarthquake.getPlace();
         String[] parts = place.split("of");
         String offset= "";
         String location = "";
@@ -53,9 +57,55 @@ public class EarthQuakeAdapter extends ArrayAdapter<Earthquake> {
 
         offsetTv.setText(offset);
         locTv.setText(location);
-        dateTv.setText(earthQuake.getDate());
-        timeTv.setText(earthQuake.getTime());
+        dateTv.setText(currentEarthquake.getDate());
+        timeTv.setText(currentEarthquake.getTime());
+
+        GradientDrawable magnitudeCircle = (GradientDrawable) magTv.getBackground();
+
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+        magnitudeCircle.setColor(magnitudeColor);
 
         return listItemView;
+    }
+
+    private int getMagnitudeColor(double magnitude) {
+        int magInt = (int) Math.floor(magnitude);
+        int magnitudeColor;
+        Log.v(LOG_TAG, "magInt = " + magInt);
+
+        switch (magInt) {
+            case 0:
+            case 1:
+                magnitudeColor = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColor = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColor = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColor = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColor = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColor = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColor = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColor = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColor = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColor = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColor);
     }
 }
